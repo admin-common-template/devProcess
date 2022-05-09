df599cf0a60cc0c1964f5b61de9669374fdce529
Kimi <752490640@qq.com>
2022-05-09 18:10:10 +0800

    first init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: '🐷',
+  description: 'note',
+  outDir: '../dist',
+  // 主题配置
+  themeConfig: {
+    // 获取每个文件最后一次 git 提交的 UNIX 时间戳(ms)，同时它将以合适的日期格式显示在每一页的底部
+    lastUpdated: true, // string | boolean
+    // 启动页面丝滑滚动
+    smoothScroll: true,
+    // 展示搜索框
+    // algolia: {
+    //   appKey: '',
+    //   indexName: '',
+    //   searchParameters: {
+    //     faeFilters: ['tags:guide,api']
+    //   }
+    // },
+    // 导航栏配置
+    nav: [
+      { text: '介绍', link: '/introduce' },
+      { text: '个人网站', link: 'http://www.ylsong.com' },
+      { text: 'Github', link: 'https://github.com/longyzw' }
+    ],
+    // 侧边栏
+    sidebar: {
+      '/': getSidebar(),
+    }
+  }
+}
+
+function getSidebar() {
+  return [
+    {
+      text: "介绍",
+      link: "/introduce"
+    },
+    {
+      text: "Admin后台系统",
+      children: [
+        {
+          text: "环境搭建",
+          children: [
+            {
+              text: "初始化项目",
+              link: "/admin/environment/setup1"
+            },
+          ]
+        },
+        {
+          text: "基础布局",
+          children: [
+            {
+              text: "Layout布局定型",
+              link: "/admin/layout/setup1"
+            }
+          ]
+        }
+      ],
+    },
+    {
+      text: "服务端",
+      link: "/server/"
+    },
+    {
+      text: "数据库",
+      link: "/sql/"
+    }
+  ]
+}
---
+++ docs/introduce.md
@@ -0,0 +1,14 @@
+# 项目介绍
+
+自建通用后台管理系统（适配PC和H5），主要面向业务层封装，目的：渲染速度更快，代码书写更简单，业务逻辑更完善。
+
+## Admin后台系统
+技术栈：vite + vue3 + pinia + vue-router + axios + Element-Plus
+
+### Admin开发流程引导
+- 1、[环境搭建](./admin/environment/setup1.md)
+- 2、[Layout布局定型](./admin/layout/setup1.md)
+
+### Server开发流程引导
+
+### SQL开发流程引导
